e8d9ec843802e9a140b22214320937c371d04b73
Aryawa Bintang <aryawabintang1999@gmail.com>
2022-12-19 22:20:35 +0800

    Add collect image file -Detect face
    Next step
    -Save image when detect face

--- collectImage.py
+++ collectImage.py
@@ -2,30 +2,37 @@
 import cv2
 
 # read the input image
-img = cv2.imread('woman.jpg')
-
-# convert to grayscale of each frames
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+cap = cv2.VideoCapture(0)
 
 # read the haarcascade to detect the faces in an image
-face_cascade = cv2.CascadeClassifier('haarcascades\haarcascade_frontalface_default.xml')
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
-# detects faces in the input image
-faces = face_cascade.detectMultiScale(gray, 1.3, 4)
-print('Number of detected faces:', len(faces))
+while(True):
+    #Capture frame by frame
+    ret, frame = cap.read()
 
-# loop over all detected faces
-if len(faces) > 0:
-   for i, (x, y, w, h) in enumerate(faces):
+    # convert to grayscale of each frames
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-      # To draw a rectangle in a face
-      cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 255), 2)
-      face = img[y:y + h, x:x + w]
-      cv2.imshow("Cropped Face", face)
-      cv2.imwrite(f'face{i}.jpg', face)
-      print(f"face{i}.jpg is saved")
+    # detects faces in the input image
+    faces = face_cascade.detectMultiScale(gray, 1.3, 4)
+    print('Number of detected faces:', len(faces))
 
-# display the image with detected faces
-cv2.imshow("image", img)
-cv2.waitKey(0)
+    # loop over all detected faces
+    # if len(faces) > 0:
+    #     for i, (x, y, w, h) in enumerate(faces):
+
+    #         # To draw a rectangle in a face
+    #         cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 255), 2)
+    #         face = frame[y:y + h, x:x + w]
+    #         cv2.imshow("Cropped Face", face)
+    #         cv2.imwrite(f'face{i}.jpg', face)
+    #         print(f"face{i}.jpg is saved")
+
+    # display the image with detected faces
+    cv2.imshow("image", frame)
+    if cv2.waitKey(20) & 0xFF == ord('q'):
+        break
+
+cap.release()
 cv2.destroyAllWindows()
